718d89da856e2b8230a26c339d635cbdf3465461
Adem Hoskin <ademjhoskin@gmail.com>
2023-10-10 09:52:18 -0400

    test modules added and command added

--- Dockerfile
+++ Dockerfile
@@ -12,11 +12,13 @@ EXPOSE 8000
 ARG DEV=false
 RUN python -m venv /py && \
     /py/bin/pip install --upgrade pip && \
+    apt-get update && \
+    apt-get install -y postgresql-client && \
+    apt-get install -y build-essential libpq-dev && \
     /py/bin/pip install -r /tmp/requirements.txt && \
-    if [ $DEV = "true" ]; \
-        then /py/bin/pip install -r /tmp/requirements.dev.txt ; \
-    fi && \
+    if [ "$DEV" = "true" ]; then /py/bin/pip install -r /tmp/requirements.dev.txt; fi && \
     rm -rf /tmp && \
+    apt-get clean && \
     adduser \
         --disabled-password \
         --no-create-home \
@@ -25,3 +27,4 @@ RUN python -m venv /py && \
 ENV PATH="/py/bin:$PATH"
 
 USER django-user
+
---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+
+from django.db.utils import OperationalError
+from django.core.management import call_command
+
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+
+class Command(BaseCommand):
+    """Django command to pause execution until database is available"""
+
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except Exception:
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available!"))
