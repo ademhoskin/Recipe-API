9430f0a58f9502488b74e0ae05073079a0dfd535
Adem Hoskin <ahoskin@gmu.edu>
2023-10-19 09:45:46 -0400

    fixed typo

--- app/core/tests.py
+++ app/core/tests.py
@@ -32,11 +32,10 @@ class ModelTests(TestCase):
     def test_create_user_with_email_success(self):
         email = 'test@example.com'
         password = 'testpass123'
-        user = get_user_model.create_user(
+        user = get_user_model.objects.create_user(
             email = email,
             password = password,
         )
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
 
