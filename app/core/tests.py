0038faa179769421a759d7422f67d4b768d23df4
Adem Hoskin <ahoskin@gmu.edu>
2023-10-20 00:01:19 -0400

    added user list testing

--- app/core/tests.py
+++ app/core/tests.py
@@ -6,6 +6,8 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
 # Create your tests here.
 
 
@@ -58,8 +60,30 @@ class ModelTests(TestCase):
 
     def test_create_superuser(self):
         user = get_user_model().objects.create_superuser(
-                'test@example.com',
-                'test123',
+            'test@example.com',
+            'test123',
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='TestUser'
+        )
+    
+    def test_user_list(self):
+        url=reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
