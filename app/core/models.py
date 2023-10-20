4b778edb09c41dbd0607469337e93b52322ccc66
Adem Hoskin <ahoskin@gmu.edu>
2023-10-19 22:37:39 -0400

    added email normalization

--- app/core/models.py
+++ app/core/models.py
@@ -9,7 +9,10 @@ from django.contrib.auth.models import (
 # Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
